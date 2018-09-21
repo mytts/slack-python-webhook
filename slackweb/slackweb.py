939271172311b78e4d974ecabb21ceaadbfb8f3b
mytts <h5h5e@foxmail.com>
2018-09-21 18:51:56 +0800

    Update slackweb.py
    use requests lib , add proxy support

--- slackweb/slackweb.py
+++ slackweb/slackweb.py
@@ -1,23 +1,11 @@
 # -*- coding: utf-8 -*-
-
-
-# 3rd party library
-try:
-    from urllib.parse import urljoin
-    from urllib.parse import urlencode
-    import urllib.request as urlrequest
-except ImportError:
-    from urlparse import urljoin
-    from urllib import urlencode
-    import urllib2 as urlrequest
-import json
-
+import requests
 
 class Slack():
 
-    def __init__(self, url=""):
+    def __init__(self, url="", proxies=None):
         self.url = url
-        self.opener = urlrequest.build_opener(urlrequest.HTTPHandler())
+        self.proxies = proxies
 
     def notify(self, **kwargs):
         """
@@ -29,8 +17,6 @@ class Slack():
         """
         Send payload to slack API
         """
-        payload_json = json.dumps(payload)
-        data = urlencode({"payload": payload_json})
-        req = urlrequest.Request(self.url)
-        response = self.opener.open(req, data.encode('utf-8')).read()
-        return response.decode('utf-8')
+        response = requests.post(self.url, json=payload, proxies=self.proxies, headers={
+                                 'Content-Type': 'application/json'})
+        return response.text
